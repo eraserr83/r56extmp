6dbc5b9e506be9b09a034ed6a297d0c45f0f307c
eraserr83 <49533185+eraserr83@users.noreply.github.com>
2019-04-14 15:48:08 +0200

    Update r56_defines.lua

--- common/defines/r56_defines.lua
+++ common/defines/r56_defines.lua
@@ -14,7 +14,7 @@ NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000				--Max air experience a country c
 NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
 NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04, 0.06,	0.08, 0.1, 0.14, 0.18, 0.22, 0.26, 0.3, 0.39, 0.48, 0.57, 0.66, 0.75, 0.78, 0.81, 0.84, 0.87, 0.9}
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
-NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
+NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 0
 
 NDefines.NAir.AIR_WING_XP_LEVELS = {20, 40, 60, 80, 100, 140, 180, 220, 260, 300, 390, 480, 570, 660, 750, 780, 810, 840, 870, 900}
 NDefines.NAir.AIR_WING_XP_TRAINING_MAX = 300.0
@@ -33,9 +33,6 @@ NDefines.NAir.DETECT_CHANCE_FROM_NIGHT = -0.8
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_SHIP_CHANCE = 0.1
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_SHIP_DAMAGE_FACTOR = 50
 NDefines.NAir.AIR_WING_XP_LOSS_REDUCTION_OVER_FRIENDLY_TERRITORY_FACTOR = 0.7
-NDefines.NAir.AA_INDUSTRY_AIR_DAMAGE_FACTOR = -0.08   --0.12				-- 5x levels = 60% defense from bombing
-NDefines.NAir.AIR_MORE_GROUND_CREWS_COST = 10			--20.0					-- CP cost to maintain more ground crews
-NDefines.NAir.AIR_MORE_GROUND_CREWS_BOOST = 0.2		--0.1					-- Efficienct boost for more ground crews
 
 NEW_COMMANDER_RANDOM_PERSONALITY_TRAIT_CHANCES = {  -- chances to gain a personality trait for new generals
 	0.80, -- 50% for first trait
